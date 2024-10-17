787ddda2bc05933189eea96745e085230f0ed694
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-10-17 14:28:04 -0300

    fix(major): Correcciones en funcionamiento de backend.

--- webPage/backend/src/server.js
+++ webPage/backend/src/server.js
@@ -1,8 +1,27 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const app = express();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const PORT = process.env.BACKEND_PORT;
+const JSON = process.env.JSON_PORT;
+
+const corsConfig = {
+    origin: `http://localhost:${JSON}`,
+    methods: ['GET', 'POST'],
+    credentials: false
+}
+
+app.use(cors(corsConfig));
+
+app.use('/api', createProxyMiddleware({
+    target: `http://localhost:${JSON}`,
+    changeOrigin: true,
+    pathRewrite: {
+        '^/api': '',
+    },
+}));
 
 app.use(express.static(path.join(__dirname, '../../frontend/public')));
 
@@ -10,7 +29,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../../frontend/public', 'index.html'));
 });
 
-// Inicia el servidor
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
